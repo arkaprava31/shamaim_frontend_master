a8e74c2b025dd3e8df8dacae454eee032dc725a0
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2025-12-05 00:43:15 +0530

    backend url add

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,7 +1,8 @@
 export const ITEMS_PER_PAGE = 10;
 // export const baseUrl = "https://helloworld-j376pdtdya-uc.a.run.app";
 // export const baseUrl="http://127.0.0.1:5001/shamaim-lifestyle/us-central1/helloWorld";
-export const baseUrl = "http://localhost:8080";
+// export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://shamaim-backend-dev2-0.onrender.com";
 // key: "rzp_test_UanPsB91bqtxk7",
 
 
